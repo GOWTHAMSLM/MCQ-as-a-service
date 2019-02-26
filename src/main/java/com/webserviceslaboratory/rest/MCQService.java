32a06db69919b1f608f88b0e074ad6c339d97db5
Sarath Kumar Selvam <sarathkumarselvam@Saraths-MacBook-Air-6.local>
2019-02-26 09:39:30 +0530

    API for register and login using MySQL database

--- src/main/java/com/webserviceslaboratory/rest/MCQService.java
+++ src/main/java/com/webserviceslaboratory/rest/MCQService.java
@@ -1,13 +1,26 @@
 package com.webserviceslaboratory.rest;
- 
+
+import com.webserviceslaboratory.data.model.User;
+
+import java.sql.*;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
  
 @Path("/MCQService")
 public class MCQService {
- 
+	
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost/MCQService";
+	// Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+    
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
@@ -17,5 +30,97 @@ public class MCQService {
 		return Response.status(200).entity(output).build();
  
 	}
- 
+	
+	@POST
+	@Path("/register")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response register(User user) {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			//Register JDBC driver
+		    Class.forName("com.mysql.jdbc.Driver");
+
+		    //Open a connection
+		    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		    //Execute a query
+		    stmt = conn.createStatement();
+		      
+		    String sql = "INSERT INTO user (full_name, email, password) VALUES ('" + user.getFullName() + "','" + user.getEmail() + "','" + user.getPassword() + "')";
+		    stmt.executeUpdate(sql);
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+
+		return Response.status(201).entity("registered").build();
+	}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response login(User user) {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			//Register JDBC driver
+		    Class.forName("com.mysql.jdbc.Driver");
+
+		    //Open a connection
+		    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		    //Execute a query
+		    stmt = conn.createStatement();
+		      
+		    String sql = "select * from user";
+		  
+		    ResultSet rs = stmt.executeQuery(sql);
+		    
+		    while(rs.next()) {
+		    	if((rs.getString("email")).equals(user.getEmail()) && (rs.getString("password")).equals(user.getPassword())) {
+		    		return Response.status(201).entity("true").build();
+		    	}
+		    }
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+
+		return Response.status(201).entity("false").build();
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<%@ page import="java.util.*, com.sun.jersey.api.client.Client, com.sun.jersey.api.client.ClientResponse, com.sun.jersey.api.client.WebResource" %>
+
+<!DOCTYPE html>
 <html>
+<head>
+<meta charset="UTF-8">
+<title>MCQ Service</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<%
+/* Client client = Client.create();
+//set the appropriate URL
+String getUrl = "http://localhost:8080/MCQ-as-a-service/rest/MCQService/sarath";
+ 
+WebResource webResource = client.resource(getUrl);
+ClientResponse clientResponse = webResource.accept("application/json").get(ClientResponse.class);
+//a successful response returns 200
+        if(clientResponse.getStatus()!=200){
+    throw new RuntimeException("HTTP Error: "+ clientResponse.getStatus());
+}
+ 
+String result = clientResponse.getEntity(String.class);
+System.out.println("Response from the Server: ");
+System.out.println(result); */
+
+Client client = Client.create();
+//Set the appropriate URL for POST request
+String postUrl = "http://localhost:8080/MCQ-as-a-service/rest/MCQService/login";
+
+  WebResource webResource = client.resource(postUrl);
+  String inputData = "{\"fullName\":\"Sarath Kumar S\",\"email\":\"sk331857@gmail.com\",\"password\":\"password\"}";
+  ClientResponse clientResponse = webResource.type("application/json").post(ClientResponse.class,inputData);
+    if(clientResponse.getStatus()!=201){
+        throw new RuntimeException("HTTP Error: "+ clientResponse.getStatus());
+    }
+     
+   String result = clientResponse.getEntity(String.class);
+   
+  
+%>
+
+<%= result %>
 </body>
 </html>
